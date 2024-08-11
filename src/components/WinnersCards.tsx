72bced22d83b8667c6d34bc5cecc4e728b6ff406
kahtab1324 <k.s.fayyad.dev@gmail.com>
2024-08-12 01:42:02 +0300

    update

--- src/components/WinnersCards.tsx
+++ src/components/WinnersCards.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Swiper as SwiperType } from "swiper";
 import "swiper/swiper-bundle.css";
