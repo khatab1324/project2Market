3d57ad1c69850044eb16b2ca2a95a71308ac8bb6
kahtab1324 <k.s.fayyad.dev@gmail.com>
2024-08-24 11:59:31 +0300

    some updates

--- src/components/WinnersCards.tsx
+++ src/components/WinnersCards.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Swiper as SwiperType } from "swiper";
 import "swiper/swiper-bundle.css";
@@ -9,19 +9,49 @@ import { winnersSessonOne } from "../constants";
 SwiperCore.use([Navigation]);
 
 export const WinnersCards: React.FC = () => {
-  // Reference for the swiper instance
   const swiperRef = useRef<SwiperType | null>(null);
+  const [showNavButtons, setShowNavButtons] = useState(true);
+  const groupSize = 3; // Adjust this depending on how many cards you want to move at once
+
+  // Function to handle navigation per group
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slideNext(groupSize);
+    }
+  };
+
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.slidePrev(groupSize);
+    }
+  };
+
+  // Check if all slides are visible
+  useEffect(() => {
+    const checkSlidesVisibility = () => {
+      if (swiperRef.current) {
+        const totalSlidesWidth =
+          swiperRef.current.slides.length *
+          swiperRef.current.slides[0].clientWidth;
+        const swiperWidth = swiperRef.current.el.clientWidth;
+
+        setShowNavButtons(totalSlidesWidth > swiperWidth);
+      }
+    };
+
+    checkSlidesVisibility();
+    window.addEventListener("resize", checkSlidesVisibility);
+
+    return () => {
+      window.removeEventListener("resize", checkSlidesVisibility);
+    };
+  }, []);
 
   return (
     <div className="relative px-4 sm:px-6 lg:px-8">
       <Swiper
-        // loop={true}
         slidesPerView={1}
         spaceBetween={20}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
         breakpoints={{
           640: {
             slidesPerView: 2,
@@ -42,13 +72,11 @@ export const WinnersCards: React.FC = () => {
             <SwiperSlide key={winner.id} className="p-3">
               <div
                 className={clsx(
-                  " feedback-card-winner px-6 sm:px-10 py-8 sm:py-5 rounded-[20px] max-w-[370px] mx-auto my-5 h-96  ",
-                 
+                  "feedback-card-winner px-6 sm:px-10 py-8 sm:py-5 rounded-[20px] max-w-[370px] mx-auto my-5 h-96"
                 )}
               >
-                {/* <FaQuoteLeft className="text-2xl sm:text-3xl text-white mb-4" /> */}
-                <div className=" mb-8 sm:mb-4 ">
-                  <h3 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[60.16px] leading-[43.16px] text-winnerGradient ">
+                <div className="mb-8 sm:mb-4">
+                  <h3 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[60.16px] leading-[43.16px] text-winnerGradient">
                     P2M TOP 1
                   </h3>
                 </div>
@@ -59,10 +87,10 @@ export const WinnersCards: React.FC = () => {
                     alt={winner.projectTitle}
                   />
                 </div>
-                <div className="flex ">
+                <div className="flex">
                   <p
                     className={clsx(
-                      "font-poppins font-semibold text-gradient text-[32px] sm:text-[32px] lg:text-[30px]  leading-[1.2] lg:leading-[76.8px] w-full  mt-8 lg:mt-4  text-silverGradient "
+                      "font-poppins font-semibold text-gradient text-[32px] sm:text-[32px] lg:text-[30px] leading-[1.2] lg:leading-[76.8px] w-full mt-8 lg:mt-4 text-silverGradient"
                     )}
                   >
                     {winner.projectTitle}
@@ -73,16 +101,20 @@ export const WinnersCards: React.FC = () => {
           ))}
         </div>
       </Swiper>
-      <div className="flex items-center absolute bottom-[170px] left-0 right-0 transform -translate-y-1/2">
-        <FaArrowCircleLeft
-          size={20}
-          className="swiper-button-prev cursor-pointer"
-        />
-        <FaArrowCircleRight
-          size={20}
-          className="swiper-button-next cursor-pointer"
-        />
-      </div>
+      {showNavButtons && (
+        <div className="flex items-center absolute bottom-[170px] left-0 right-0 transform -translate-y-1/2">
+          <FaArrowCircleLeft
+            size={20}
+            className="swiper-button-prev cursor-pointer"
+            onClick={handlePrev}
+          />
+          <FaArrowCircleRight
+            size={20}
+            className="swiper-button-next cursor-pointer"
+            onClick={handleNext}
+          />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,16 +1,77 @@
+import { useEffect, useState } from "react";
 import { statsOfTheCompetition } from "../constants";
-export const StatsComponent = () => {
+
+interface Stat {
+  id: string;
+  title: string;
+  value: string;
+}
+
+interface AnimatedValue {
+  id: string;
+  value: number;
+}
+
+const useAnimateValues = (stats: Stat[]): AnimatedValue[] => {
+  const [animatedValues, setAnimatedValues] = useState<AnimatedValue[]>(
+    stats.map((state) => ({
+      id: state.id,
+      value: 0,
+    }))
+  );
+
+  useEffect(() => {
+    const animateValue = (
+      start: number,
+      end: number,
+      duration: number,
+      callback: (value: number) => void
+    ) => {
+      let startTimestamp: number | null = null;
+      const step = (timestamp: number) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        callback(Math.floor(progress * (end - start) + start));
+        if (progress < 1) {
+          window.requestAnimationFrame(step);
+        }
+      };
+      window.requestAnimationFrame(step);
+    };
+
+    stats.forEach((state) => {
+      // Convert state.value to a number
+      const endValue = parseInt(state.value, 10);
+      animateValue(0, endValue, 2000, (newValue) => {
+        setAnimatedValues((prevValues) =>
+          prevValues.map((val) =>
+            val.id === state.id ? { ...val, value: newValue } : val
+          )
+        );
+      });
+    });
+  }, [stats]);
+
+  return animatedValues;
+};
+
+export const StatsComponent: React.FC = () => {
+  const animatedValues = useAnimateValues(statsOfTheCompetition);
+
   return (
-    <div className="flex flex-row  flex-wrap xs:justify-center justify-start ss:mb-20 mb-16 ss:mt-20 mt-16 lg:justify-around xl:justify-between">
-      {statsOfTheCompetition.map((state) => (
+    <div className="flex flex-row flex-wrap xs:justify-center justify-start ss:mb-20 mb-16 ss:mt-20 mt-16 lg:justify-around xl:justify-between">
+      {animatedValues.map((state) => (
         <div
           key={state.id}
-          className="flex  justify-center items-center sm:mr-10 m-4"
+          className="flex justify-center items-center sm:mr-10 m-4"
         >
           <h3 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[60.16px] leading-[43.16px] text-white">
-            {state.title}:
+            {statsOfTheCompetition.find((s) => s.id === state.id)?.title}:
           </h3>
-          <p className="font-poppins font-normal xs:text-[38.89px] text-[20.89px] xs:leading-[38.89px] leading-[40.16px] text-gradient uppercase ml-4">
+          <p
+            className="font-poppins font-normal xs:text-[38.89px] text-[20.89px] xs:leading-[38.89px] leading-[40.16px] text-gradient uppercase ml-4 transition-all duration-500 ease-out"
+            style={{ transitionProperty: "opacity, transform" }}
+          >
             {state.value}
           </p>
         </div>
